8098b40ffbb46cc9c21d6aa6ab62650aeda86c67
Ali moghimi <a.moghimi@Brx.com>
2022-06-01 13:41:20 +0430

    تغییرات جزیی

--- packages/theme/pages/Category.vue
+++ packages/theme/pages/Category.vue
@@ -12,7 +12,7 @@
                         onclick="document.getElementById('modal-search').style.display='none'">
                         <i class="fa fa-close"></i>
                     </button>
-                    <div class="remodal-page-header">لوازم جانبی گوشی موبایل</div>
+                    <div class="remodal-page-header">لوازم جانبی </div>
                     <div class="remodal-page-main">
                         <div class="filter-bar">
                             <button class="js-listing-options-clear">پاک کردن همه</button>
@@ -63,37 +63,6 @@
                                     <div class="toggle-box-active">
                                         <ul>
                                             <li class="has-sub">
-                                                <a href="#">دسته بندی</a>
-                                                <ul class="catalog-list">
-                                                    <li><a href="#" class="catalog-link"><i
-                                                                class="fa fa-angle-left"></i>کالای دیجیتال</a>
-                                                        <div class="show-more">
-                                                            <span class="catalog-cat-item"><i
-                                                                    class="fa fa-angle-down"></i>لوازم جانبی گوشی</span>
-                                                            <span class="catalog-cat-item"><i
-                                                                    class="fa fa-angle-down"></i>لوازم جانبی گوشی
-                                                                موبایل</span>
-                                                            <ul class="catalog-list">
-                                                                <li><a href="#" class="catalog-link"> کیف و کاور
-                                                                        </a></li>
-                                                                <li><a href="#" class="catalog-link">محافظ صفحه نمایش
-                                                                        گوشی</a></li>
-                                                                <li><a href="#" class="catalog-link">هندزفری</a></li>
-                                                            </ul>
-                                                        </div>
-                                                    </li>
-                                                </ul>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="filter">
-                                <div class="toggle-box">
-                                    <div class="toggle-box-active">
-                                        <ul>
-                                            <li class="has-sub">
                                                 <a href="#">برند</a>
                                                 <ul>
                                                     <li>
@@ -134,7 +103,7 @@
                                                                     <span class="ui-checkbox-check"></span>
                                                                 </label>
                                                                 <label for="remember"
-                                                                    class="remember-me">دیجی‌استور</label>
+                                                                    class="remember-me">بروکس</label>
                                                             </div>
                                                         </a>
                                                     </li>
@@ -148,7 +117,7 @@
                                                                     <span class="ui-checkbox-check"></span>
                                                                 </label>
                                                                 <label for="remember"
-                                                                    class="remember-me">دیجی‌استور</label>
+                                                                    class="remember-me">بروکس</label>
                                                             </div>
                                                         </a>
                                                     </li>
@@ -161,7 +130,7 @@
                                                                     <span class="ui-checkbox-check"></span>
                                                                 </label>
                                                                 <label for="remember"
-                                                                    class="remember-me">دیجی‌استور</label>
+                                                                    class="remember-me">بروکس</label>
                                                             </div>
                                                         </a>
                                                     </li>
