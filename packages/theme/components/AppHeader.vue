85e01e218818caf500836b574d157568988b304f
mehdi sahd <mehdishad01@gmail.com>
2022-06-06 15:37:17 +0430

    appHeaderFirstCommit

--- packages/theme/components/AppHeader.vue
+++ packages/theme/components/AppHeader.vue
@@ -717,137 +717,130 @@
 </template>
 
 <script>
-import MiniCartHeader from './Header/MiniCart.vue'
-import MiniCartResponsive from './Header/MiniCartResponsive.vue'
-import Categories from './Header/Categories.vue'
-import HeaderProfile from './Header/HeaderProfile.vue'
-import HeaderProfileResponsive from './Header/HeaderProfileResponsive.vue'
-import SearchBox from './Header/SearchBox.vue'
-import { SfHeader, SfImage, SfIcon, SfButton, SfBadge, SfSearchBar, SfOverlay } from '@storefront-ui/vue';
-import { useUiState } from '~/composables';
-import { useCart, useFacet, useUser, cartGetters, useWishlist, wishlistGetters } from '@vue-storefront/spree';
-import { computed, ref, watch, onBeforeUnmount, useRouter } from '@nuxtjs/composition-api';
-import { useUiHelpers } from '~/composables';
-import LocaleSelector from './LocaleSelector';
-import SearchResults from '~/components/SearchResults';
-import HeaderNavigation from './HeaderNavigation';
-import { clickOutside } from '@storefront-ui/vue/src/utilities/directives/click-outside/click-outside-directive.js';
 import {
-  mapMobileObserver,
-  unMapMobileObserver
-} from '@storefront-ui/vue/src/utilities/mobile-observer.js';
-import debounce from 'lodash.debounce';
+  SfSidebar,
+  SfButton,
+  SfList,
+  SfIcon,
+  SfHeading,
+  SfMenuItem,
+  SfFilter,
+  SfProductCard,
+  SfProductCardHorizontal,
+  SfPagination,
+  SfAccordion,
+  SfSelect,
+  SfBreadcrumbs,
+  SfLoader,
+  SfColor,
+  SfProperty
+} from '@storefront-ui/vue';
+import { computed, onMounted, useContext } from '@nuxtjs/composition-api';
+import { useCart, useWishlist, productGetters, useFacet, facetGetters, useUser, wishlistGetters, useMenus } from '@vue-storefront/spree';
+import { useUiHelpers, useUiState } from '~/composables';
+import { onSSR } from '@vue-storefront/core';
+import LazyHydrate from 'vue-lazy-hydration';
+import cacheControl from './../helpers/cacheControl';
+import CategoryPageHeader from '~/components/CategoryPageHeader';
 
+// TODO(addToCart qty, horizontal): https://github.com/vuestorefront/storefront-ui/issues/1606
 export default {
-  components: {
-    MiniCartHeader,
-    MiniCartResponsive,
-    Categories,
-    HeaderProfile,
-    HeaderProfileResponsive,
-    SearchBox,
-    SfHeader,
-    SfImage,
-    LocaleSelector,
-    SfIcon,
-    SfButton,
-    SfBadge,
-    SfSearchBar,
-    SearchResults,
-    SfOverlay,
-    HeaderNavigation
-  },
-  directives: { clickOutside },
-  setup(props, { root }) {
-    const router = useRouter();
-    const { toggleCartSidebar, toggleWishlistSidebar, toggleLoginModal, isMobileMenuOpen } = useUiState();
-    const { setTermForUrl, getFacetsFromURL } = useUiHelpers();
+  transition: 'fade',
+  middleware: cacheControl({
+    'max-age': 60,
+    'stale-when-revalidate': 5
+  }),
+  setup() {
+    const th = useUiHelpers();
+    const uiState = useUiState();
+    const context = useContext();
+    const { addItem: addItemToCart, isInCart } = useCart();
+    const { result, search, loading, error } = useFacet();
+    const { wishlist, addItem: addItemToWishlist, isInWishlist, removeItem: removeItemFromWishlist } = useWishlist();
     const { isAuthenticated } = useUser();
-    const { result: searchResult, search } = useFacet('searchResults');
-    const { cart } = useCart();
-    const { wishlist } = useWishlist();
-    const term = ref(getFacetsFromURL().phrase);
-    const isSearchOpen = ref(false);
-    const searchBarRef = ref(null);
-    const isMobile = computed(mapMobileObserver().isMobile);
+    const { menu, loadMenu } = useMenus('header');
+    const products = computed(() => facetGetters.getProducts(result.value));
+    const breadcrumbs = computed(() => facetGetters.getBreadcrumbs(result.value).map(e => ({...e, link: context.localePath(e.link)})));
+    const pagination = computed(() => facetGetters.getPagination(result.value));
+    const categoryTree = computed(() => facetGetters.getCategoryTree(result.value));
+    const { locale } = context.app.i18n;
 
-    const result = computed(() => searchResult.value?.data);
-    const cartTotalItems = computed(() => cartGetters.getTotalItems(cart.value));
-    const wishlistTotalItems = computed(() => wishlistGetters.getTotalItems(wishlist.value));
+    const getRoute = (category) => {
+      // if (menu.value.isDisabled) {
+         return '/c/' + category.slug;
+      // }
+      //return "";
+    };
 
-    const accountIcon = computed(() => isAuthenticated.value ? 'profile_fill' : 'profile');
+    const activeCategory = computed(() => {
+      const items = categoryTree.value.items;
+
+      if (!items || !items.length) {
+        return '';
+      }
+
+      const category = items.find(({ isCurrent, items }) => isCurrent || items.find(({ isCurrent }) => isCurrent));
+      return category?.label || items[0]?.label;
+    });
+
     const isWishlistDisabled = computed(() => wishlistGetters.isWishlistDisabled(wishlist.value));
 
-    // TODO: https://github.com/DivanteLtd/vue-storefront/issues/4927
-    const handleAccountClick = async () => {
-      if (isAuthenticated.value) {
-        const localeAccountPath = root.localePath({ name: 'my-account' });
-        return router.push(localeAccountPath);
-      }
-
-      toggleLoginModal();
-    };
-
-    const closeSearch = () => {
-      const wishlistClassName = 'sf-product-card__wishlist-icon';
-      const isWishlistIconClicked = event.path.find(p => wishlistClassName.search(p.className) > 0);
-      if (isWishlistIconClicked || !isSearchOpen.value) return;
-
-      term.value = '';
-      isSearchOpen.value = false;
-    };
-
-    const handleSearch = debounce(async (paramValue) => {
-      if (!paramValue.target) {
-        term.value = paramValue;
+    const handleWishlistClick = async (product) => {
+      if (!isAuthenticated.value) {
+        uiState.toggleLoginModal();
+      } else if (!isInWishlist({ product })) {
+        await addItemToWishlist({ product });
       } else {
-        term.value = paramValue.target.value;
-      }
-      await search({ term: term.value });
-    }, 1000);
-
-    const closeOrFocusSearchBar = () => {
-      if (isMobile.value) {
-        return closeSearch();
-      } else {
-        term.value = '';
-        return searchBarRef.value.$el.children[0].focus();
+        await removeItemFromWishlist({ product });
       }
     };
 
-    watch(() => term.value, (newVal, oldVal) => {
-      const shouldSearchBeOpened = (!isMobile.value && term.value.length > 0) && ((!oldVal && newVal) || (newVal.length !== oldVal.length && isSearchOpen.value === false));
-      if (shouldSearchBeOpened) {
-        isSearchOpen.value = true;
-      }
+    onMounted(async () => {
+      await loadMenu({menuType: 'header', menuName: 'Main menu', locale: locale});
     });
 
-    const removeSearchResults = () => {};
-
-    onBeforeUnmount(() => {
-      unMapMobileObserver();
+    onSSR(async () => {
+      await search(th.getFacetsFromURL());
+      if (error?.value?.search) context.app.nuxt.error;
     });
-
     return {
-      accountIcon,
-      cartTotalItems,
-      wishlistTotalItems,
-      handleAccountClick,
-      toggleCartSidebar,
-      toggleWishlistSidebar,
-      setTermForUrl,
-      term,
-      isSearchOpen,
-      closeSearch,
-      handleSearch,
-      result,
-      closeOrFocusSearchBar,
-      searchBarRef,
-      isMobile,
-      isMobileMenuOpen,
-      removeSearchResults,
-      isWishlistDisabled
+      ...uiState,
+      th,
+      products,
+      categoryTree,
+      loading,
+      productGetters,
+      pagination,
+      activeCategory,
+      breadcrumbs,
+      addItemToCart,
+      isInWishlist,
+      isInCart,
+      handleWishlistClick,
+      isWishlistDisabled,
+      getRoute,
+      menu
     };
+  },
+  components: {
+    CategoryPageHeader,
+    SfButton,
+    SfSidebar,
+    SfIcon,
+    SfList,
+    SfFilter,
+    SfProductCard,
+    SfProductCardHorizontal,
+    SfPagination,
+    SfMenuItem,
+    SfAccordion,
+    SfSelect,
+    SfBreadcrumbs,
+    SfLoader,
+    SfColor,
+    SfHeading,
+    SfProperty,
+    LazyHydrate
   }
 };
 </script>
