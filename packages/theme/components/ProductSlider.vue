7611c836a5e0afbd59a038be7fa3749d8b8d81c3
Ali moghimi <a.moghimi@Brx.com>
2022-06-21 18:21:42 +0430

    اضافه کردن بدون عکس ها

--- packages/theme/components/ProductSlider.vue
+++ packages/theme/components/ProductSlider.vue
@@ -32,7 +32,7 @@
                   <div class="item">
                     <a :href="localePath(getProductLink(product))">
                       <img
-                        :src="productGetters.getCoverImage(product) || '/images/product/Panel-18w-min-1.png'"
+                        :src="productGetters.getCoverImage(product) || '/images/product/Panel-18w-min-2.png'"
                         class="img-fluid"
                         alt="img-slider"
                       />
