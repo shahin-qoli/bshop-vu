f91c52f3088981fa0a1858faad75f2d620c51c7e
MehdiShad <mehdishad01@gmail.com>
2022-06-23 13:17:34 +0430

    add row for Categories

--- packages/theme/components/Header/Categories.vue
+++ packages/theme/components/Header/Categories.vue
@@ -9,26 +9,36 @@
        class="list-category-menu-2"
         >{{cat_lev1.name}}</a
       >
-      <ul   
-      class="megamenu-level-3">
-        <li 
+        <div   
+        class="row">
+        <div
          v-for="(cat_lev2) in cat_lev1.items" 
          :key="cat_lev2.id"  
+          class="col-4"
+        >
+
+        </div>
+        <div class="col-4"></div>
+        <li 
+
          class="list-category row" 
          style="padding:25px">
           <a :href="cat_lev2.link" class="list-category-megamenu row"
             >{{cat_lev2.name}}</a
           >
-          <ul  
-            v-for="(cat_lev3) in cat_lev2.items"
-            :key="cat_lev3.id"
-            class="item-megamenu-item">
-            <a :href="'/c/'+cat_lev1.slug" class="list-category-megamenu-3"
-              ><span style="color:grey">{{cat_lev3.name}}</span>
-            </a>
-          </ul>        
+          <ul>
+            <li  
+              v-for="(cat_lev3) in cat_lev2.items"
+              :key="cat_lev3.id"
+              class="item-megamenu-item"> 
+              <a :href="cat_lev3.link" class="list-category-megamenu-3"
+                ><span style="color:grey">{{cat_lev3.name}}</span>
+              </a>
+            </li>  
+          </ul>
+      
         </li>
-      </ul>
+      </div>
     </li>
   </ul>
 </template>
